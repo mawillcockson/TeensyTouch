b65db6be0fae2dcfc0dfa1c5770dc23643644132
mawillcockson <mawillcockson@googlemail.com>
2016-01-21 14:07:01 -0600

    Working on improving use of this file
    In the middle of writing out setup_tsi() functions.
    
    Still need to figure out how to use interrupts.

--- TeensyTouch.c
+++ TeensyTouch.c
@@ -4,6 +4,7 @@
 
 #include <kinetis.h>
 #include <pins_arduino.h>
+#include <stdint.h>
 
 #if defined(KINETISK)
 #define TSI_GENCS_LPCLKS_VAL    ((TSI0_GENCS << 3)  >> 31)
@@ -37,60 +38,84 @@
 #define TSI_SCANC_AMPSC_VAL         ((TSI0_SCANC << 29) >> 30)
 #define TSI_PEN_LPSP_VAL        ((TSI0_PEN << 12) >> 28)
 #define TSI_PEN_LPSP(n)         (((n) & 15) << 16)
+/* Teensy: 16, ??, ??, ??, ??, 33, 17, 19, 18, 0, 1,  32, 25, 15, 22, 23 */
+/* Chip:   0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 */
 #define TSI_PEN_PEN15_VAL       ((TSI0_PEN << 16) >> 31)
-#define TSI_PEN_PEN15_EN        (0x8000) // (1 << 15)
+#define TSI_PEN_PEN15_EN        (0x8000) // (1 << 15) // 23
+#define PIN23_EN                (TSI_PEN_PEN15_EN)
 #define TSI_PEN_PEN14_VAL       ((TSI0_PEN << 17) >> 31)
-#define TSI_PEN_PEN14_EN        (0x4000) // (1 << 14)
+#define TSI_PEN_PEN14_EN        (0x4000) // (1 << 14) // 22
+#define PIN22_EN                (TSI_PEN_PEN14_EN)
 #define TSI_PEN_PEN13_VAL       ((TSI0_PEN << 18) >> 31)
-#define TSI_PEN_PEN13_EN        (0x2000) // (1 << 13)
+#define TSI_PEN_PEN13_EN        (0x2000) // (1 << 13) // 15
+#define PIN15_EN                (TSI_PEN_PEN13_EN)
 #define TSI_PEN_PEN12_VAL       ((TSI0_PEN << 19) >> 31)
-#define TSI_PEN_PEN12_EN        (0x1000) // (1 << 12)
+#define TSI_PEN_PEN12_EN        (0x1000) // (1 << 12) // 25
+#define PIN25_EN                (TSI_PEN_PEN12_EN)
 #define TSI_PEN_PEN11_VAL       ((TSI0_PEN << 20) >> 31)
-#define TSI_PEN_PEN11_EN        (0x800) // (1 << 11)
+#define TSI_PEN_PEN11_EN        (0x800) // (1 << 11)  // 32
+#define PIN32_EN                (TSI_PEN_PEN11_EN)
 #define TSI_PEN_PEN10_VAL       ((TSI0_PEN << 21) >> 31)
-#define TSI_PEN_PEN10_EN        (0x400) // (1 << 10)
+#define TSI_PEN_PEN10_EN        (0x400) // (1 << 10)  // 1
+#define PIN1_EN                (TSI_PEN_PEN10_EN)
 #define TSI_PEN_PEN9_VAL        ((TSI0_PEN << 22) >> 31)
-#define TSI_PEN_PEN9_EN        (0x200) // (1 << 9)
+#define TSI_PEN_PEN9_EN        (0x200) // (1 << 9)    // 0
+#define PIN0_EN                (TSI_PEN_PEN9_EN)
 #define TSI_PEN_PEN8_VAL        ((TSI0_PEN << 23) >> 31)
-#define TSI_PEN_PEN8_EN        (0x100) // (1 << 8)
+#define TSI_PEN_PEN8_EN        (0x100) // (1 << 8)    // 18
+#define PIN18_EN                (TSI_PEN_PEN8_EN)
 #define TSI_PEN_PEN7_VAL        ((TSI0_PEN << 24) >> 31)
-#define TSI_PEN_PEN7_EN        (0x80) // (1 << 7)
+#define TSI_PEN_PEN7_EN        (0x80) // (1 << 7)     // 19
+#define PIN19_EN                (TSI_PEN_PEN7_EN)
 #define TSI_PEN_PEN6_VAL        ((TSI0_PEN << 25) >> 31)
-#define TSI_PEN_PEN6_EN        (0x40) // (1 << 6)
+#define TSI_PEN_PEN6_EN        (0x40) // (1 << 6)     // 17
+#define PIN17_EN                (TSI_PEN_PEN6_EN)
 #define TSI_PEN_PEN5_VAL        ((TSI0_PEN << 26) >> 31)
-#define TSI_PEN_PEN5_EN        (0x20) // (1 << 5)
+#define TSI_PEN_PEN5_EN        (0x20) // (1 << 5)     // 33
+#define PIN33_EN                (TSI_PEN_PEN4_EN)
 #define TSI_PEN_PEN4_VAL        ((TSI0_PEN << 27) >> 31)
-#define TSI_PEN_PEN4_EN        (0x10) // (1 << 4)
+#define TSI_PEN_PEN4_EN        (0x10) // (1 << 4)     // ??
 #define TSI_PEN_PEN3_VAL        ((TSI0_PEN << 28) >> 31)
-#define TSI_PEN_PEN3_EN        (0x8) // (1 << 3)
+#define TSI_PEN_PEN3_EN        (0x8) // (1 << 3)      // ??
 #define TSI_PEN_PEN2_VAL        ((TSI0_PEN << 29) >> 31)
-#define TSI_PEN_PEN2_EN        (0x4) // (1 << 2)
+#define TSI_PEN_PEN2_EN        (0x4) // (1 << 2)      // ??
 #define TSI_PEN_PEN1_VAL        ((TSI0_PEN << 30) >> 31)
-#define TSI_PEN_PEN1_EN        (0x2) // (1 << 1)
+#define TSI_PEN_PEN1_EN        (0x2) // (1 << 1)      // ??
 #define TSI_PEN_PEN0_VAL        ((TSI0_PEN << 31) >> 31)
-#define TSI_PEN_PEN0_EN        (0x1) // (1 << 0)
+#define TSI_PEN_PEN0_EN        (0x1) // (1 << 0)      // 16
+#define PIN16_EN                (TSI_PEN_PEN0_EN)
 #define TSI_PEN_PEN_VALs        ((TSI0_PEN << 16) >> 16)
 #define TSI_WUCNTR_VAL              ((TSI0_WUCNTR << 16) >> 16)
 #define TSI0_CNTR1_CTN_VAL      ((TSI0_CNTR1        ) >> 16)
-#define TSI0_CNTR1_CTN1_VAL     ((TSI0_CNTR1   << 16) >> 16)
+#define TSI0_CNTR1_CTN1_VAL     ((TSI0_CNTR1   << 16) >> 16) // 16
 #define TSI0_CNTR3_CTN_VAL      ((TSI0_CNTR3        ) >> 16)
 #define TSI0_CNTR3_CTN1_VAL     ((TSI0_CNTR3   << 16) >> 16)
 #define TSI0_CNTR5_CTN_VAL      ((TSI0_CNTR5        ) >> 16)
 #define TSI0_CNTR5_CTN1_VAL     ((TSI0_CNTR5   << 16) >> 16)
-#define TSI0_CNTR7_CTN_VAL      ((TSI0_CNTR7        ) >> 16)
-#define TSI0_CNTR7_CTN1_VAL     ((TSI0_CNTR7   << 16) >> 16)
-#define TSI0_CNTR9_CTN_VAL      ((TSI0_CNTR9        ) >> 16)
-#define TSI0_CNTR9_CTN1_VAL     ((TSI0_CNTR9   << 16) >> 16)
+#define TSI0_CNTR7_CTN_VAL      ((TSI0_CNTR7        ) >> 16) // 19
+#define TSI0_CNTR7_CTN1_VAL     ((TSI0_CNTR7   << 16) >> 16) // 17
+#define TSI0_CNTR9_CTN_VAL      ((TSI0_CNTR9        ) >> 16) // 0
+#define TSI0_CNTR9_CTN1_VAL     ((TSI0_CNTR9   << 16) >> 16) // 18
 #define TSI0_CNTR11_CTN_VAL     ((TSI0_CNTR11      ) >> 16)
-#define TSI0_CNTR11_CTN1_VAL    ((TSI0_CNTR11 << 16) >> 16)
-#define TSI0_CNTR13_CTN_VAL     ((TSI0_CNTR13     ) >> 16)
+#define TSI0_CNTR11_CTN1_VAL    ((TSI0_CNTR11 << 16) >> 16) // 1
+#define TSI0_CNTR13_CTN_VAL     ((TSI0_CNTR13     ) >> 16) // 15
 #define TSI0_CNTR13_CTN1_VAL    ((TSI0_CNTR13 << 16) >> 16)
-#define TSI0_CNTR15_CTN_VAL     ((TSI0_CNTR15      ) >> 16)
-#define TSI0_CNTR15_CTN1_VAL    ((TSI0_CNTR15 << 16) >> 16)
+#define TSI0_CNTR15_CTN_VAL     ((TSI0_CNTR15      ) >> 16) // 23
+#define TSI0_CNTR15_CTN1_VAL    ((TSI0_CNTR15 << 16) >> 16) // 22
 #define TSI_THRESHOLD_LTHH_VAL          ((TSI0_THRESHOLD      ) >> 16)
 #define TSI_THRESHOLD_LTHH(n)           (((n) & 0xFFFF) << 16) // Valid values: {0,65535}
 #define TSI_THRESHOLD_HTHH_VAL          ((TSI0_THRESHOLD << 16) >> 16)
 #define TSI_THRESHOLD_HTHH(n)           ((n) & 0xFFFF) // Valid values: {0,65535}
+
+#define PIN23_VAL (TSI0_CNTR15_CTN_VAL)
+#define PIN22_VAL (TSI0_CNTR15_CTN1_VAL)
+#define PIN19_VAL (TSI0_CNTR7_CTN_VAL)
+#define PIN18_VAL (TSI0_CNTR9_CTN1_VAL)
+#define PIN17_VAL (TSI0_CNTR7_CTN1_VAL)
+#define PIN16_VAL (TSI0_CNTR1_CTN1_VAL)
+#define PIN15_VAL (TSI0_CNTR13_CTN_VAL)
+#define PIN1_VAL (TSI0_CNTR11_CTN1_VAL)
+#define PIN0_VAL (TSI0_CNTR9_CTN_VAL)
 #endif
 
 #if (!defined(TSI_PERIODIC))
@@ -107,4 +132,92 @@ void tsi_start(void) {
     *portConfigRegister(0) = PORT_PCR_MUX(0); // Need to figure out what this line does
     SIM_SCGC5 |= SIM_SCGC5_TSI;               // And this one, too
     TSI0_GENCS = TSI0_GENCS | TSI_GENCS_TSIEN;
+}
+
+#define Spn(a) (Serial.println(a))
+enum SETUP_ERROR_CODES {NORMAL,
+                        OUT_OF_RANGE_VALUE};
+void interpret_setup_error_codes(SETUP_ERROR_CODES error_number) {
+    switch(error_number) {
+        case NORMAL: Spn("Normal");
+        case OUT_OF_RANGE_VALUE: Spn("A value passed as an argument was not within its valid range");
+    }
+}
+    
+
+#if defined(TEENSYTOUCH_DEFINES)
+SETUP_ERROR_CODES setup_tsi(
+    (uint16_t) pen_en, /* This is set up by OR-ing (|) together the
+                        * PIN0_EN - PIN23_EN defines provided, and
+                        * casting as uint16_t
+                        */
+    uint8_t number_of_scans, /* Defines the number of scans the TSI
+                              * module should perform on each electrode
+                              */
+    uint8_t prescaler, /* Defines the value by which the TSI module's
+                        * reference clock's frequency will be divided,
+                        * as compared to the system clock
+                        */
+    ) {
+    /* TSI module needs to be stopped/disabled in order to change some of its register values */
+    tsi_stop();
+    /* Next all flag bits are reset by writing 1 to them */
+    TSI0_GENCS |= TSI_GENCS_EOSF_RST(1);   // Valid values: {0,1}
+    TSI0_GENCS |= TSI_GENCS_OUTRGF_RST(1); // Valid values: {0,1}
+    TSI0_GENCS |= TSI_GENCS_EXTERF_RST(1); // Valid values: {0,1}
+    TSI0_GENCS |= TSI_GENCS_OVRF_RST(1);   // Valid values: {0,1}
+    /* Haven't set up low-power modes yet, so this register bit is kept
+     * sensible for non-low-power moder
+     */
+    TSI0_GENCS |= TSI_GENCS_ESOR(1);       // Valid values: {0,1}
+    
+    
+    
+}
+
+#else
+enum TSI_READ_MODES {HARDWARE_POLL,
+                     SOFTWARE_POLL,
+                     SOFTWARE_TRIGGER};
+
+SETUP_ERROR_CODES setup_tsi(
+    (uint16_t) pen_en, /* This is set up by OR-ing (|) together the
+                        * PIN0_EN - PIN23_EN defines provided, and
+                        * casting as uint16_t
+                        */
+    uint8_t number_of_scans, /* Defines the number of scans the TSI
+                              * module should perform on each electrode.
+                              * Valid value: 1 - 32
+                              */
+    uint8_t prescaler, /* Defines the value by which the TSI module's
+                        * reference clock's frequency will be divided,
+                        * as compared to the system clock.
+                        * Valid values: 1 - 128, by powers of 2
+                        *   (e.g. 2^(0), 2^(1), 2^(2), ...)
+                        */
+    uint8_t reference_current, /* Defines the reference oscillator
+                                * charging current, which when raised,
+                                * makes the electrode charge faster, but
+                                * also makes it less sensitive to finer
+                                * differences in capacitance.
+                                * Valid values: Even numbers starting at 2
+                                *   (e.g. 2, 4, 6, ...) in microAmps
+    /* Instead of picking the software/hardware triggering, it's set up by picking the mode in the last argument */
+    uint8_t 
+    TSI_READ_MODES(mode)) {
+    /* TSI module needs to be stopped/disabled in order to change some of its register values */
+    tsi_stop();
+    /* Next all flag bits are reset by writing 1 to them */
+    TSI0_GENCS |= TSI_GENCS_EOSF_RST(1);   // Valid values: {0,1}
+    TSI0_GENCS |= TSI_GENCS_OUTRGF_RST(1); // Valid values: {0,1}
+    TSI0_GENCS |= TSI_GENCS_EXTERF_RST(1); // Valid values: {0,1}
+    TSI0_GENCS |= TSI_GENCS_OVRF_RST(1);   // Valid values: {0,1}
+    /* Haven't set up low-power modes yet, so this register bit is kept
+     * sensible for non-low-power moder
+     */
+    /* End-of-scan interrupt instead of out-of-range */
+    TSI0_GENCS |= TSI_GENCS_ESOR(1);       // Valid values: {0,1}
+    /* Stop TSI module in low-power modes instead of running it in all */
+    TSI0_GENCS |= TSI_GENCS_STPE(1);       // Valid values: {0,1}
+    
 }
